20c33772435302878d73e1e9668547e701317fbc
Harshkumarsingh123 <harshrajputofficial800@gmail.com>
2024-12-27 00:16:34 +0530

    Literals in Js

---
+++ Literals.js
@@ -0,0 +1,14 @@
+let a=123;//Number 
+let b=1n//BigInt
+let c=false; //boolean
+let d=null;//Undefined
+let e="Harsh"-2; //Nan
+const str="Harsh"; //String
+const name=`Harsh`; //backtick
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(d);
+console.log(e);
+console.log(Symbol(str))
+console.log(`Good morning ${name}`);
